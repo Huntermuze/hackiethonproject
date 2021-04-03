eff82e42c0baaf973cba64bec7b52ba6a360865c
Gyp1127 <yupin.gan@gmail.com>
2021-04-03 15:24:03 +0800

    joke api

--- health/jokeApi.js
+++ health/jokeApi.js
@@ -1,29 +1,30 @@
 const api_url = "https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", api_url);
+function getJoke() {
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", api_url);   
 
-xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
-    {
-        var randomJoke = JSON.parse(xhr.responseText);
-
-        if(randomJoke.type == "single")
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status < 300)
         {
-            // If type == "single", the joke only has the "joke" property
-            alert(randomJoke.joke);
+            var randomJoke = JSON.parse(xhr.responseText);
+    
+            if(randomJoke.type == "single")
+            {
+                document.getElementById("joke").innerHTML = randomJoke.joke;
+            }
+            else
+            {
+                document.getElementById("joke").innerHTML = randomJoke.setup + "\n"+ randomJoke.delivery;
+            }
         }
-        else
+        else if(xhr.readyState == 4)
         {
-            // If type == "single", the joke only has the "joke" property
-            alert(randomJoke.setup);
-            alert(randomJoke.delivery);
+            alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
         }
-    }
-    else if(xhr.readyState == 4)
-    {
-        alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
-    }
+    };
+    
+    xhr.send();
 };
 
-xhr.send();
