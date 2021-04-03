a8772bc1836518485dec35417540f348371eca80
Gyp1127 <yupin.gan@gmail.com>
2021-04-03 18:12:17 +0800

    broken weather api

--- health/Api.js
+++ health/Api.js
@@ -50,8 +50,29 @@ function getQuote() {
     };
     
     request.send();
-
-
-    
-
 }
+
+//WEATHER API CODE BELOW
+
+const APIKEY = "c0c7ea2535a2adfcbeab069590cc5a32"
+('api.openweathermap.org/data/2.5/weather?q='+ inputValue.value +'&appid='+ APIKEY)
+
+//class fetch {
+//    async getCurrent(input)
+//}
+
+
+
+
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+    fetch('api.openweathermap.org/data/2.5/weather?q='+ inputValue.value +'&appid='+ APIKEY)
+    .then(response => response.json())
+    .then(data => console.log(data))
+.catch(err => alert("Not a valid City!"))
+}); 
