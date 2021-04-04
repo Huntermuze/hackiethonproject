74e901c851a672c1c710bb9feaada7b78187e8e9
Gyp1127 <yupin.gan@gmail.com>
2021-04-04 18:48:51 +0800

    working weather, broken map

--- health/Api.js
+++ health/Api.js
@@ -56,62 +56,83 @@ function getQuote() {
 
 //WEATHER API CODE BELOW
 
-const mapsKey = "AIzaSyBoaOgoUiA8gxcbUsnNpbpG4XT3QX4GO7s";
-
-
-// Attach your callback function to the `window` object
-//window.initMap = function() {
-  // JS API is loaded and available
-//};
-
-// Append the 'script' element to 'head'
-//document.head.appendChild(script);
+const mapsKey = "AIzaSyBoaOgoUiA8gxcbUsnNpbpG4XT3QX4GO7s"; 
 
 function getWeather() {
+
     var inputValue = document.querySelector('#user-city');
-    var longitude;
-    var latitude;
     var weather_url = "http://api.weatherapi.com/v1/forecast.json?key=f7d495448f004fd3a0661541210404&q=" + inputValue.value +"&days=1&aqi=no&alerts=no";
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", weather_url);  
+    xhr.open("GET", weather_url);   
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status < 300)
-        { //If weather api doesn't throw an error, it fetches and replaces html
+        {
             var weather = JSON.parse(xhr.responseText);
-            document.getElementById("w-name").innerHTML = weather["location"]["name"];
+            document.getElementById("w-name").innerHTML = weather["location"]["name"]
             document.getElementById("w-temp").innerHTML = weather["current"]["temp_c"] + "&#8451;";
             document.getElementById("w-rain").innerHTML = weather["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"] + "%";
             document.getElementById("w-snow").innerHTML = weather["forecast"]["forecastday"][0]["day"]["daily_chance_of_snow"] + "%";
-            longitude = weather["location"]["lon"];
-            latitude = weather["location"]["lat"];
-            
-            // Create the script tag, set the appropriate attributes
-           // var script = document.createElement('script');
-           // script.src = 'https://maps.googleapis.com/maps/api/js?key='+ mapsKey +'&callback=initMap';
-           // script.async = true;
-            
-            // Append the 'script' element to 'head'
-          //  document.head.appendChild(script);
-
-            //drawing of map
-          //  window.initMap = function() {
-          //      var options = {
-          //          zoom: 12,
-          //          center: {lat: latitude, lng: longitude}
-         //      }
-            
-           //     var map = new google.maps.Map(document.getElementById(google-maps))
-         //   }
             
         }
         else if(xhr.readyState == 4)
         {
-            alert(xhr.responseText);
+            alert("Error while requesting weather.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
         }
-    }
-}; 
+    };
+    
+    xhr.send();
+};
+
+
+
+// function getWeather() {
+//     var inputValue = document.querySelector('#user-city');
+//  //   var longitude;
+//  //   var latitude;
+//     var weather_url = "http://api.weatherapi.com/v1/forecast.json?key=f7d495448f004fd3a0661541210404&q=" + inputValue.value +"&days=1&aqi=no&alerts=no";
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", weather_url);  
+
+//     xhr.onreadystatechange = function() {
+//         if(xhr.readyState == 4 && xhr.status < 300)
+//         { //If weather api doesn't throw an error, it fetches and replaces html
+//             var weather = JSON.parse(xhr.responseText);
+//             console.log(weather);
+//             document.getElementById("w-name").innerHTML = weather["location"]["name"];
+//             document.getElementById("w-temp").innerHTML = weather["current"]["temp_c"] + "&#8451;";
+//             document.getElementById("w-rain").innerHTML = weather["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"] + "%";
+//             document.getElementById("w-snow").innerHTML = weather["forecast"]["forecastday"][0]["day"]["daily_chance_of_snow"] + "%";
+//            // longitude = weather["location"]["lon"];
+//            // latitude = weather["location"]["lat"];
+            
+//             // Create the script tag, set the appropriate attributes
+//            // var script = document.createElement('script');
+//            // script.src = 'https://maps.googleapis.com/maps/api/js?key='+ mapsKey +'&callback=initMap';
+//            // script.async = true;
+            
+//             // Append the 'script' element to 'head'
+//           //  document.head.appendChild(script);
+
+//             //drawing of map
+//           //  window.initMap = function() {
+//           //      var options = {
+//           //          zoom: 12,
+//           //          center: {lat: latitude, lng: longitude}
+//          //      }
+            
+//            //     var map = new google.maps.Map(document.getElementById(google-maps))
+//          //   }
+            
+//         }
+//         else if(xhr.readyState == 4)
+//         {
+//             alert(xhr.responseText);
+//         }
+//     }
+// }; 
 
 
 
